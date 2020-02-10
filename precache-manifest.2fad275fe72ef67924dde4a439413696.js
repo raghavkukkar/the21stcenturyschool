c5450edfc9dc31209b18aee7a05542e2c98df803
Raghav <raghavkukkar9@gmail.com>
2020-02-10 18:29:34 +0530

    Updates

---
+++ precache-manifest.2fad275fe72ef67924dde4a439413696.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fbb78f516e7ee42b84857d5d7167e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d54083eae9249f5b8896",
+    "url": "/static/css/main.4726ebbe.chunk.css"
+  },
+  {
+    "revision": "c62b998502acf278596b",
+    "url": "/static/js/2.ad9a600f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ad9a600f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54083eae9249f5b8896",
+    "url": "/static/js/main.02ca7234.chunk.js"
+  },
+  {
+    "revision": "269d59ebd887228c0f56",
+    "url": "/static/js/runtime-main.55551b08.js"
+  },
+  {
+    "revision": "1e78fd2b1de0309356a2ea1cfa1fe38e",
+    "url": "/static/media/gear.1e78fd2b.png"
+  },
+  {
+    "revision": "0c816f9f400106db86b232183fd3f129",
+    "url": "/static/media/hardware.0c816f9f.jpg"
+  },
+  {
+    "revision": "67bea4a2a90fca316d0274cfff7d0730",
+    "url": "/static/media/logo.67bea4a2.png"
+  },
+  {
+    "revision": "f6dd3a7ab618fbd2661b701642f442e5",
+    "url": "/static/media/mainbg.f6dd3a7a.jpeg"
+  },
+  {
+    "revision": "91882a1cdc8069e18111efe8d62b91f0",
+    "url": "/static/media/photo.91882a1c.png"
+  },
+  {
+    "revision": "03e15418ebbf73c4f2af3a78e62a9e04",
+    "url": "/static/media/teach.03e15418.jpg"
+  }
+]);
